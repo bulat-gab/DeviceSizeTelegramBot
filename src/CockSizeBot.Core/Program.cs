181f1e78d9f79fe94be7edede61e03faed00a248
bulat-gab <gabdrakhmanov.br@gmail.com>
2022-03-14 15:41:57 +0700

    Add Telegram bot boilerplate code

--- src/CockSizeBot.Core/Program.cs
+++ src/CockSizeBot.Core/Program.cs
@@ -1,5 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Serilog;
+using Telegram.Bot;
 
+Log.Logger = new LoggerConfiguration()
+            .MinimumLevel.Debug()
+            .WriteTo.Console()
+            .WriteTo.File($"logs\\log-{DateTime.UtcNow.ToString("dd/MM/yyyy")}.log",
+                rollingInterval: RollingInterval.Day)
+            .CreateLogger();
 
-   
+Log.Information("Serilog initialized");
+
+var token = Environment.GetEnvironmentVariable("TelegramBotToken", EnvironmentVariableTarget.Machine);
+if (token == null)
+{
+    Log.Error("Could not fetch Telegram Bot Token from Environment Variables");
+    return;
+}
+
+var botClient = new TelegramBotClient(token);
+
+var me = await botClient.GetMeAsync();
+Console.WriteLine($"Hello, World! I am user {me.Id} and my name is {me.FirstName}.");
